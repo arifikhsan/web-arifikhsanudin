91bd5af72c83f0c603ca60e12290c559ebc78528
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-22 18:36:10 +0700

    content done

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let now = new Date();
+
+let greeting = ''
+
+if (now.getHours() < 12 ) {
+  greeting = 'Good Morning! ヾ(•ω•`)o`'
+} else if (now.getHours() < 15) {
+  greeting = 'Good afternoon! ( •̀ ω •́ )✧'
+} else if (now.getHours() < 21) {
+  greeting = 'Good evening! (o゜▽゜)o☆'
+} else {
+  greeting = 'Good night! (✿◡‿◡)'
+}
+
+let divTime = document.getElementById("time");
+let pGreeting = document.createElement('p')
+let pTime = document.createElement("p");
+
+pGreeting.innerHTML = greeting
+divTime.appendChild(pGreeting);
+
+console.log(now.getHours())
+
