b7d018c29158f6a8bd9a51c8d4f44a096bc7df2b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-22 19:23:48 +0700

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
 let now = new Date();
 
-let greeting = ''
+// top header
+let greeting = "";
 
-if (now.getHours() < 12 ) {
-  greeting = 'Good Morning! ヾ(•ω•`)o`'
+if (now.getHours() < 12) {
+  greeting = "Good Morning!   ヾ(•ω•`)o`";
 } else if (now.getHours() < 15) {
-  greeting = 'Good afternoon! ( •̀ ω •́ )✧'
+  greeting = "Good afternoon!   ( •̀ ω •́ )✧";
 } else if (now.getHours() < 21) {
-  greeting = 'Good evening! (o゜▽゜)o☆'
+  greeting = "Good evening!   (o゜▽゜)o☆";
 } else {
-  greeting = 'Good night! (✿◡‿◡)'
+  greeting = "Good night!   (✿◡‿◡)";
 }
 
 let divTime = document.getElementById("time");
-let pGreeting = document.createElement('p')
+let pGreeting = document.createElement("p");
 let pTime = document.createElement("p");
 
-pGreeting.innerHTML = greeting
+pGreeting.innerHTML = greeting;
 divTime.appendChild(pGreeting);
+
+
+// footer
+let footer = document.getElementsByTagName("footer")[0];
+
+let spanYear = document.createElement("span");
+spanYear.innerHTML = now.getFullYear();
+footer.appendChild(spanYear);
